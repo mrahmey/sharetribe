c67e46eee24b3f5b17ea264936b6dde5501abbcf
Mikko Koski <mikkokos@gmail.com>
2015-03-20 16:59:16 +0200

    Populate listings.unit_type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150320091305) do
+ActiveRecord::Schema.define(:version => 20150320144657) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
